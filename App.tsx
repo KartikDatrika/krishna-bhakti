f6dc9e546d36357f256c584dfd436757957639dc
Kartik Datrika <Datrika.Kartik@Honeywell.com>
2020-09-29 00:50:10 +0530

    Radical Change Actions on tab1 screen.

--- App.tsx
+++ App.tsx
@@ -5,16 +5,18 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import { Button, ImageBackground, ImageSourcePropType, StyleProp, ViewStyle } from 'react-native';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
-
+  
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
       <SafeAreaProvider>
+        <Button onPress={()=>{}} title={"p"}/>
         <Navigation colorScheme={colorScheme} />
         <StatusBar />
       </SafeAreaProvider>
